0f9fbf6dda3b480d91ce6c440d12046204f03e80
asksmcauk <asksmca.uk@gmail.com>
2024-03-17 15:12:08 +0000

    swagger and try catch added

--- app.py
+++ app.py
@@ -3,13 +3,29 @@ from flask import Flask, request, render_template
 
 import requests
 import validators
+
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL="/swagger"
+API_URL="/static/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Access API'
+    }
+)
 app = Flask(__name__)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/websitestatus', methods=['POST'])
+#API to check the website health status
+@app.route('/websitestatus', methods=['POST','GET'])
 def check_websitestatus():
     file = request.files['inputFile']
 
@@ -24,12 +40,13 @@ def check_websitestatus():
     
     validation = validators.url(data['url'])
     if validation != True:
-        return 'Invalid URL value'
-
-    uResponse = requests.get(data['url'])
-
+        return 'Invalid URL.'
+    try:
+        uResponse = requests.get(data['url'])
+    except Exception:
+        return "Web Application is Not Found or not Working."
     if uResponse.status_code == 200:
-        return str(uResponse.status_code)
+        return "HTTP request is successful."
     else:
         return "HTTP request is unsuccessful,Please check"
 
